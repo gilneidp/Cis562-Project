fd7e3c9ee4496f6e122353b162a4e6b71b81c2cf
gilneidp <gilnei@gilnei.com.br>
2014-12-04 12:20:32 -0600

    date

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
             <div class="form-group">
                 <div class='input-group date' id='datetimepicker'>
                     <input type='text' class="form-control" />
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar" id="datetimepicker"></span>
                     </span>
                 </div>
             </div>
