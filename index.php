1db6a5ed7f430520636f3a1b8d046d4e247f6e1c
gilneidp <gilnei@gilnei.com.br>
2014-12-04 01:10:45 -0600

    Weather

--- index.php
+++ index.php
@@ -26,14 +26,10 @@
 		</div>
 	       <div class="row">
 		   <div class="col-md-4">
-		   <Module>
-<ModulePrefs title="Weather Widget " author="ibh2000" author_email="hema.ibh2012@gmail.com" directory_title="Weather Widget" title_url="http://www.gmodules.com/ig/ifr?url=http://hosting.gmodules.com/ig/gadgets/file/104221389754720193778/costco_coupons.xml" description="Put the date and the city and you get the temperature " thumbnail="http://learningwithcomputers.pbworks.com/f/1144682903/weather%20widget.jpg" screenshot="http://learningwithcomputers.pbworks.com/f/1144682903/weather%20widget.jpg" height="740" width="330" scrolling="true" category="communication" category2="tools"/>
-<Content type="html">
-<![CDATA[
-</head> <body> </center> <div dir="ltr" style="text-align: left;" trbidi="on"> <div style="text-align: center;"> <script type="text/javascript"><!-- google_ad_client = "ca-pub-9090022083155147"; /* kim 22 */ google_ad_slot = "5376666460"; google_ad_width = 300; google_ad_height = 250; //--> </script> <script src="//pagead2.googlesyndication.com/pagead/show_ads.js" type="text/javascript"> </script> <script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-49946278-1', 'gmodules.com'); ga('send', 'pageview'); </script> </div> <div dir="ltr" style="text-align: center;" trbidi="on"> <div> <script type="text/javascript" id="WolframAlphaScriptf1c9bbbd53cd5be6e08084369702b6d5" src="http://www.wolframalpha.com/widget/widget.jsp?id=f1c9bbbd53cd5be6e08084369702b6d5&output=popup"></script> <div style="text-align: center;"> <script type="text/javascript"><!-- google_ad_client = "ca-pub-9090022083155147"; /* kim4 */ google_ad_slot = "5724556069"; google_ad_width = 300; google_ad_height = 250; //--> </script> <script src="http://pagead2.googlesyndication.com/pagead/show_ads.js" type="text/javascript"> </script> </div> <div class="addthis_toolbox addthis_default_style "> <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a> <a class="addthis_button_tweet"></a> <a class="addthis_button_pinterest_pinit" pi:pinit:layout="horizontal"></a> <a class="addthis_counter addthis_pill_style"></a> </div> <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52a1402c7f9d5c78"></script> </div> </div> </center> </body>
-]]>
-</Content>
-</Module>
+		   <div id="cont_a227fc726fe88f454735c570837ae6c8">
+  <span id="h_a227fc726fe88f454735c570837ae6c8"><a id="a_a227fc726fe88f454735c570837ae6c8" href="http://www.theweather.com/" target="_blank" rel="nofollow" style="color:#808080;font-family:Helvetica;font-size:14px;">Weather</a> Manhattan</span>
+  <script type="text/javascript" src="http://www.theweather.com/wid_loader/a227fc726fe88f454735c570837ae6c8"></script>
+</div>
 		   </div>
 		   </div>
   
