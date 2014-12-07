d5d0216a46c13cb1a9cd46bc6081fc81a985d069
gilneidp <gilnei@gilnei.com.br>
2014-12-06 18:52:08 -0600

    Crazy

--- index.php
+++ index.php
@@ -98,7 +98,43 @@
 		   
 		</div>
 		<div class="col-md-12"><!--Map Space-->
-		<div id="googleMap" style="width:1000px;height:380px;"></div>
+		<!DOCTYPE html>
+<html>
+<head>
+<script
+src="http://maps.googleapis.com/maps/api/js">
+</script>
+
+<script>
+var myCenter=new google.maps.LatLng(51.508742,-0.120850);
+var marker;
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:5,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+marker=new google.maps.Marker({
+  position:myCenter,
+  animation:google.maps.Animation.BOUNCE
+  });
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+</head>
+
+<body>
+<div id="googleMap" style="width:1000px;height:380px;"></div>
+</body>
+</html>
 		</div> <!--End Map Space-->
   
   
