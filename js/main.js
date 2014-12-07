679acf2708f1847ff98a5543f7a45f108bd21b1d
gilneidp <gilnei@gilnei.com.br>
2014-12-07 14:49:09 -0600

    Icon

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {
-  $.get("menu.html", function(data) { 
-    $("#navigation-menu").append(data); 
-    var page = window.location.href.slice(window.location.href.lastIndexOf("/")+1);
-    $("a[href*='" + page + "']").parent().addClass("active");
-  });
-});
 
 $(document).ready(function() {
   $.get("maps.html", function(data) { 
