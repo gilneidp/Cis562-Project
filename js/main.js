0af67b91c7bfbf3015a37e0ff2ff95a24217bba8
gilneidp <gilnei@gilnei.com.br>
2014-12-06 18:32:29 -0600

    Maps

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ $(document).ready(function() {
     $("a[href*='" + page + "']").parent().addClass("active");
   });
 });
+
+$(document).ready(function() {
+  $.get("maps.html", function(data) { 
+    $("#googleMap").append(data); 
+    var page = window.location.href.slice(window.location.href.lastIndexOf("/")+1);
+    $("a[href*='" + page + "']").parent().addClass("active");
+  });
+});
