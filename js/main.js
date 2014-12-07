061a305026190d968e28311796961cf110f6b6aa
gilneidp <gilnei@gilnei.com.br>
2014-12-06 19:49:02 -0600

    Maps

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@ $(document).ready(function() {
   });
 });
 
-
+$(document).ready(function() {
+  $.get("maps.html", function(data) { 
+    $("#googleMap").append(data); 
+    var page = window.location.href.slice(window.location.href.lastIndexOf("/")+1);
+    $("a[href*='" + page + "']").parent().addClass("active");
+  });
+});
